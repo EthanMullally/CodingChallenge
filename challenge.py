02f9b62a315c8d6235e982996a3541ba3297364c
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-07-15 21:53:18 -0400

    Create challenge.py

---
+++ challenge.py
@@ -0,0 +1,49 @@
+import pgzrun
+import random
+
+WIDTH = 600
+HEIGHT = 400
+score = 0
+lives = 10
+
+fox = Actor("player")
+fox.pos= 400,200
+fox_speed = 20
+coin = Actor("coin")
+coin.pos = 500,100 
+
+def draw():
+  global game_over
+  if not game_over:
+    screen.fill("green")
+    player.draw()
+    coin.draw()
+  else:
+    screen.draw.text("Game Over! Press R to restart", color="white", topleft=("20,30"))
+def place_coin():
+  global coin
+  global WIDTH
+  global HEIGHT
+  coin.pos = random.randint((WIDTH, HEIGHT))
+  
+def update():
+  global player_speed
+  global score 
+  global game_over
+  global lives
+  global fox
+  coin_hit = player.colliderect(coin)
+  if coin_hit:
+    score+=1
+    place_coin()
+  if lives == 0:
+    game_over = True
+  if not game_over:
+    if keyboard.a:
+      player.y = player.y+player_speed
+    if keyboard.d:
+      player.x = player.x-player_speed
+  else:
+    if keyboard.r:
+      game_over = False
+      fox.pos = random.randint((WIDTH, HEIGHT))
