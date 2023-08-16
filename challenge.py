6dbcdb89f4c861778fa259f7d79e7c23c7a13dbd
clipyplusplus <124309800+clipyplusplus@users.noreply.github.com>
2023-08-15 21:53:41 -0400

    Update challenge.py

--- challenge.py
+++ challenge.py
@@ -10,22 +10,27 @@ fox = Actor("player")
 fox.pos= 400,200
 
 coin = Actor("coin")
-coin.pos = 500,100 
+coin.pos = random.randint(WIDTH, HEIGHT)
+
+enemy = Actor("enemy")
+enemy.pos = random.randint(WIDTH, HEIGHT)
 def draw():
   global game_over
   global score
   if not game_over:
     screen.fill("green")
-    screen.draw.text("Score:" + str(score), topleft=(20,20))
+    screen.draw.text("Score: " + str(score) + "/Lives: " + str(lives), color="white", topleft=(20,20))
     player.draw()
     coin.draw()
+    enemy.draw()
   else:
     screen.draw.text("Game Over! Press R to restart", color="white", topleft=(20,20))
+    player.draw()
 def place_coin():
   global coin
   global WIDTH
   global HEIGHT
-  coin.pos = random.randint((WIDTH, HEIGHT))
+  coin.x = random.randint(WIDTH, HEIGHT)
   
 def update():
   global player_speed
@@ -37,15 +42,31 @@ def update():
   if coin_hit:
     score+=1
     place_coin()
-  if lives == 0:
-    game_over = True
   if not game_over:
     fox_speed = 20
     if keyboard.a:
       player.y = player.y+fox_speed
     if keyboard.d:
       player.x = player.x-fox_speed
+    player_hit_enemy = fox.colliderect(enemy)
+    if player_hit_enemy:
+      if lives > 0:
+        lives = lives-1
+      else:
+        game_over = True
+    def move_enemy():
+      if random.choice([True, False]):
+        random_move = random.randint(1,2)
+        if random_move == 1:
+          enemy.x = enemy.x+20
+        else:
+          enemy.x = enemy.x-20
+      else:
+        move_enemy()
   else:
+    fox.x = fox.x-400
     if keyboard.r:
       game_over = False
-      fox.pos = random.randint((WIDTH, HEIGHT))
+      fox.pos = random.randint(WIDTH, HEIGHT)
+  move_enemy()
+pgzrun.go()
